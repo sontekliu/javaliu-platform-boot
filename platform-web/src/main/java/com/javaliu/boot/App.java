eafce776e51a09686d6abeb697539423ee177c20
sontek <sontek@yeah.net>
2019-01-29 17:32:19 +0800

    init project

---
+++ platform-web/src/main/java/com/javaliu/boot/App.java
@@ -0,0 +1,14 @@
+package com.javaliu.boot;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class App extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(App.class);
+    }
+}
